149eb2f22f29f11e665dbbc42f23931605da8fb0
raja327 <nrajaram327@gmail.com>
2025-04-11 09:10:13 +0545

    create product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { PAGE_SIZE } from '../constants';
 import { connectToDatabase } from '../db';
 import Product, { IProduct } from '../db/models/product.model';
 
@@ -51,6 +52,45 @@ async function getProductsByTag({
     .sort({ createdAt: 'desc' })
     .limit(limit);
   return JSON.parse(JSON.stringify(products)) as IProduct[];
+}
+
+// Get one product by slug
+export async function getProductBySlug(slug: string) {
+  await connectToDatabase();
+  const product = await Product.findOne({ slug, isPublished: true });
+  if (!product) throw new Error('Product not found');
+  return JSON.parse(JSON.stringify(product)) as IProduct;
+}
+
+// get related products :products with same category
+export async function getRelatedProductsByCategory({
+  category,
+  productId,
+  limit = PAGE_SIZE,
+  page = 1,
+}: {
+  category: string;
+  productId: string;
+  limit?: number;
+  page: number;
+}) {
+  await connectToDatabase();
+  const skipAmount = (Number(page) - 1) * limit;
+  const conditions = {
+    isPublished: true,
+    category,
+    _id: { $ne: productId },
+  };
+
+  const products = await Product.find(conditions)
+    .sort({ numSales: 'desc' })
+    .skip(skipAmount)
+    .limit(limit);
+  const productsCount = await Product.countDocuments(conditions);
+  return {
+    data: JSON.parse(JSON.stringify(products)) as IProduct[],
+    totalPages: Math.ceil(productsCount / limit),
+  };
 }
 
 export { getAllCategories, getProductsForCard, getProductsByTag };
