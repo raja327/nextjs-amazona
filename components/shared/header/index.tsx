e13c692849a3d749c4accffb5bbf300c5eb160e0
raja327 <nrajaram327@gmail.com>
2025-04-28 15:32:31 +0545

    create-categories-sidebar

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Search from './search';
 
-import { Button } from '@/components/ui/button';
-import { MenuIcon } from 'lucide-react';
 import data from '@/lib/data';
 import Menu from './menu';
+import Sidebar from './sidebar';
+import { getAllCategories } from '@/lib/actions/product.actions';
 
-export default function Header() {
+export default async function Header() {
+  const categories = await getAllCategories();
   return (
     <header className="bg-black text-white">
       <div className="px-2">
@@ -33,13 +34,7 @@ export default function Header() {
           <Menu />
         </div>
         <div className="flex items-center px-3 mb-[1px] bg-gray-800">
-          <Button
-            variant={'ghost'}
-            className="dark header-button flex items-center gap-1 text-base [&_svg]:size-6"
-          >
-            <MenuIcon />
-            All
-          </Button>
+          <Sidebar categories={categories} />
           <div className="flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px]">
             {data.headerMenus.map((menu) => (
               <Link
---
+++ components/shared/header/sidebar.tsx
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { X, ChevronRight, UserCircle, MenuIcon } from 'lucide-react';
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { auth } from '@/auth';
+import { Button } from '@/components/ui/button';
+import { SignOut } from '@/lib/actions/user.actions';
+
+export default async function Sidebar({
+  categories,
+}: {
+  categories: string[];
+}) {
+  const session = await auth();
+  return (
+    <Drawer direction="left">
+      <DrawerTrigger className="header-button flex gap-2 items-center !p-2">
+        <MenuIcon className="flex flex-col h-full" />
+        All
+      </DrawerTrigger>
+      <DrawerContent className="w-[350px] mt-0 top-0">
+        <div className="flex flex-col h-full">
+          {/* User Sign In Section */}
+          <div className="dark bg-gray-800 text-foreground flex items-center justify-between">
+            <DrawerHeader>
+              <DrawerTitle className="flex items-center">
+                <UserCircle className="h-6 w-6 mr-2" />
+                {session ? (
+                  <DrawerClose asChild>
+                    <Link href={'/account'}>
+                      <span className="text-lg font-semibold">
+                        hello,{session.user.name}
+                      </span>
+                    </Link>
+                  </DrawerClose>
+                ) : (
+                  <DrawerClose asChild>
+                    <Link href={'/sign-in'}>
+                      <span className="text-lg font-semibold">
+                        Hello,sign-in
+                      </span>
+                    </Link>
+                  </DrawerClose>
+                )}
+              </DrawerTitle>
+              <DrawerDescription></DrawerDescription>
+            </DrawerHeader>
+            <DrawerClose asChild>
+              <Button variant="ghost" size="icon" className="mr-2">
+                <X className="h-5 w-5" />
+                <span className="sr-only">Close</span>
+              </Button>
+            </DrawerClose>
+          </div>
+          {/* Shop by category */}
+          <div className="flex-1 overflow-y-auto">
+            <div className="p-4 border-b">
+              <h2 className="text-lg font-semibold">Shop By Department</h2>
+            </div>
+            <nav className="flex flex-col">
+              {categories.map((category) => (
+                <DrawerClose asChild key={category}>
+                  <Link
+                    className={`flex items-center justify-between item-button`}
+                    href={`/search?category=${category}`}
+                  >
+                    <span>{category}</span>
+                    <ChevronRight className="h-4 w-4" />
+                  </Link>
+                </DrawerClose>
+              ))}
+            </nav>
+          </div>
+          {/* setting and help */}
+          <div className="border-t flex flex-col">
+            <div className="p-4">
+              <h2 className="text-lg font-semibold">Help & Settings</h2>
+            </div>
+            <DrawerClose asChild>
+              <Link href={`/page/customer-service`} className="item-button">
+                Customer Service
+              </Link>
+            </DrawerClose>
+            {session ? (
+              <form action={SignOut} className="w-full">
+                <Button
+                  className="w-full justify-start item-button text-base"
+                  variant={'ghost'}
+                >
+                  Sign Out
+                </Button>
+              </form>
+            ) : (
+              <Link href={'/sign-in'} className="item-button">
+                Sign in
+              </Link>
+            )}
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
